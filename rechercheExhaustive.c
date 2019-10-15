da17cd6e5f21d64bbfdc5dcf4d91f2655539798e
Julien <julien.gustin@student.uliege.be>
2019-10-15 12:58:30 +0200

    Amélioration recherche exhaustive

--- rechercheExhaustive.c
+++ rechercheExhaustive.c
@@ -3,109 +3,61 @@
 #include <time.h>
 #include "rechercheExhaustive.h"
 
-static void switchVal(char *tab, int x, int y);
-/* switch le contenu de tab[x] et tab[y] */
+static void calculScoreExtreme(char *tab, int taille, int *scoresExtremes){
+  int nbrN = 0;
+  int stockageN = 0;
+  int nbrP = 0;
+  int stockageP = 0;
 
-static void mettreXDernierePos(char *tab, int tailleTab, int *check);
-/* permet de mettre tout les 'p' à la fin du tableau afin d'exploiter tout les possibilités du tableau */
+  for(int k = taille; k >= 0; k--){
+    if(tab[k] == 'n'){
+      nbrN++;
+      stockageP += nbrP;
+    }
 
+    else{
+      nbrP++;
+      stockageN += nbrN;
+    }
+  }
 
-static void switchVal(char *tab, int x, int y){
-  char tmp = tab[y];
-  tab[y] = tab[x];
-  tab[x] = tmp;
-
+  if(abs(stockageN - stockageP) >= 45)
+    (*scoresExtremes)++;
+  return;
 }
 
-static void mettreXDernierePos(char *tab, int tailleTab, int *check){
-  int pos = -1;
 
-  for(int i = 0 ; i < tailleTab ;i++){
-    //printf("[%c]", tab[i]);
-    if(i != 0){ // tab[-1] n'existe pas
-      if(tab[i-1] == 'n' && tab[i] == 'p')
-        pos = i;
-    }
+static void reccExhaustive(int n, int p, int i, char *tab, char c, int *compteur, int *scoresExtremes){
 
-    if(tab[i] == 'p')
-      *check = i;
+  if(i >= 0)
+    tab[i] = c;
 
+  if(n+p == 0){
+    /*printf("\n%d : ", *compteur);
+    for(int j = 0; j <= i; j++)
+      printf("[%c]", tab[j]);*/
+
+    calculScoreExtreme(tab, i, scoresExtremes);
+
+    (*compteur)++;
+    return;
   }
 
-  if(pos != -1){
-    switchVal(tab, pos-1, pos);
+  if(n != 0)
+    reccExhaustive(n-1, p, i+1, tab, 'n', compteur, scoresExtremes);
 
-    int j = pos +1;
-    int k = tailleTab-1;
+  if(p != 0)
+    reccExhaustive(n, p-1, i+1, tab, 'p', compteur, scoresExtremes);
 
-    while(j < k){
-      switchVal(tab, j, k);
-      j++;
-      k--;
-
-    }
-  }
 }
 
 
 void exhaustive(int n, int p){
-  // init variable
-
-  int nbrN;
-  int nbrP;
-  int stockageN;
-  int stockageP;
-  int j = 0;
-  int check;
+  int compteur = 0;
   int scoresExtremes = 0;
+  char tab[n+p];
 
-  // fin init variable
+  reccExhaustive(n, p, -1, tab, '/', &compteur, &scoresExtremes);
 
-  char *tab = malloc(sizeof(char) *n+p); //crée un tableau de la taille du nombre de n + le nombre de p
-
-  for(int i = 0; i < n+p ; i++){
-
-    if(i < n)
-      tab[i] = 'n';
-
-    else
-      tab[i] = 'p';
-  }
-  //printf("\n");
-    //printf("\n");
-
-  //printf("\n");
-
-
-  while(check != p-1){ //s'arrete si le dernier p est à la position du nombre de p ce qui veut dire que le tableau à été inverser [n][p][p] => [p][p][n] et donc toute les possibilités de tab ont été  crée
-
-    nbrN = 0;
-    nbrP = 0;
-    stockageN = 0;
-    stockageP = 0;
-      //printf("%d: ", j+1);
-    mettreXDernierePos(tab, n+p, &check);
-    for(int i = n+p-1; i >= 0; i--){
-      if(tab[i] == 'n'){
-        nbrN++;
-        stockageP += nbrP;
-      }
-
-      else{
-        nbrP++;
-        stockageN += nbrN;
-      }
-    }
-
-    if(abs(stockageN - stockageP) >= 45)
-      scoresExtremes++;
-    //printf("\n");
-    j++;
-
-    }
-
-    printf("\n score extremes %d  sur %d \n ce qui correspond à %f %%", scoresExtremes, j, (float)(scoresExtremes)/(float)(j)*100);
-    free(tab);
-
-
+  printf("\n score extremes %d  sur %d \n ce qui correspond à %f %%", scoresExtremes, compteur, (float)(scoresExtremes)/(float)(compteur)*100);
 }
